00c1842a95a37cceba8d8fa217faf21feaec3d02
AnimXer01 <157260909+AnimXer01@users.noreply.github.com>
2024-02-23 18:49:28 +0800

    Update service.jsx

--- src/utilities/service.jsx
+++ src/utilities/service.jsx
@@ -1,3 +1,3 @@
 export const baseUrl = "https://animxera-me.vercel.app/meta/anilist"
 export const baseMangaUrl = 'https://animxera-me.vercel.app/meta/anilist-manga'
-export const animeUrl = "https://animxera-me.vercel.app/"
+export const animeUrl = "https://animxera-me.vercel.app"
